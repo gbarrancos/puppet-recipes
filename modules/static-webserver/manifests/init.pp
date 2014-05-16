8ca6e71f2355bceb7811a8249c066418e6fb635f
Gustavo Barrancos <gbarrancos@gmail.com>
2014-05-16 18:35:42 -0300

    nginx logrotate settings

--- modules/static-webserver/manifests/init.pp
+++ modules/static-webserver/manifests/init.pp
@@ -8,13 +8,23 @@ class static-webserver {
     mode => 644,
     source => 'puppet:///modules/static-webserver/main_site',
   }
-  
+ 
+  package { 'logrotate':
+    ensure => installed,
+  }
+ 
   service { 'nginx':
     ensure => running,
     restart => '',
   }
+  
+  file { '/etc/logrotate.d/nginx':
+   owner => root,
+   group => root,
+   ensure => file,
+   mode => 644,
+   source => 'puppet:///modules/static-webserver/nginx_logrotate'
 
- # TODO:
- # - logrotate
+  }
 
 }
